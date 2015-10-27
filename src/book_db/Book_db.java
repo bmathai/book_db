051ddb4dc92d326e78b44eb54ec477bd4eea88b6
sherlocktech <sean.stamm5@gmail.com>
2015-10-27 09:08:06 -0400

    read in file and split into separate tokens

--- src/book_db/Book_db.java
+++ src/book_db/Book_db.java
@@ -5,6 +5,10 @@
  */
 package book_db;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  *
  * @author Sean
@@ -14,8 +18,25 @@ public class Book_db {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws FileNotFoundException{
+        
+        String fileName = "C:\\Users\\Sean\\Documents\\NetBeansProjects\\book_db\\BookFile";
+        String line = null;
+        
+        Scanner infile = new Scanner(new File(fileName));
+        
+        while(infile.hasNextLine()) {
+            line = infile.nextLine();
+            //create array of individual entries without commas
+            String [] tokens = line.split(",");
+            for (String token : tokens) {
+                //add tokens to proper file, tab delimited
+            }
+            System.out.println(line); //test to see if reading file correctly
+        }
+        
+        
     }
-    
 }
+    
+
