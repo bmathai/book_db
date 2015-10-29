c5e54ee04bb7d491a5f0ea6b278ce4cebda480fc
Drew Proctor <alprocto@gmail.com>
2015-10-29 09:08:06 -0400

    add classes

---
+++ src/book_db/Books.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package book_db;
+
+import java.util.*;
+
+/**
+ *
+ * @author alprocto
+ */
+public class Books {
+    public static ArrayList<Books> books = new ArrayList();
+    public int isbn;
+    public String title;
+}
