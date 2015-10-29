41dcc47b6b34a141091e2102005ad9e4e3b7dfc2
Drew Proctor <alprocto@gmail.com>
2015-10-29 11:29:48 -0400

    create author and import data

--- src/book_db/Author.java
+++ src/book_db/Author.java
@@ -12,19 +12,55 @@ import java.util.*;
  * @author alprocto
  */
 public class Author {
-  public static ArrayList<Author> authors = new ArrayList();
-  public int authorId;
-  public String authorLname;
-  public String authorFname;
-    
-  
-  public static int getId(String lastName, String firstName){
-      for(int i = 0; i<authors.size(); i++){
-          Author temp = authors.get(i);
-          if(temp.authorLname.equals(lastName) && temp.authorFname.equals(firstName)){
-              return temp.authorId;
-          }
-      }
-      return -1;
-  }
+
+    public static ArrayList<Author> authors = new ArrayList();
+    public int authorId;
+    public String authorLname;
+    public String authorFname;
+
+    public static int getId(String lastName, String firstName) {
+        for (int i = 0; i < authors.size(); i++) {
+            Author temp = authors.get(i);
+            if (temp.authorLname.equals(lastName) && temp.authorFname.equals(firstName)) {
+                return temp.authorId;
+            }
+        }
+        return -1;
+    }
+
+    public static int createAuthor(String lastName, String firstName, boolean allowDuplicateAuthor) {
+        Author newAuthor = new Author();
+        if (allowDuplicateAuthor) {
+            newAuthor.authorLname = lastName;
+            newAuthor.authorFname = firstName;
+            newAuthor.authorId = authors.size();
+            authors.add(newAuthor);
+            return newAuthor.authorId;
+        } else {
+            boolean check = false;
+            for (int i = 0; i < authors.size(); i++) {
+                Author temp = authors.get(i);
+                if (temp.authorLname.equals(lastName) && temp.authorFname.equals(firstName)) {
+                    check = true;
+                }
+            }
+            if (!check) {
+                newAuthor.authorLname = lastName;
+                newAuthor.authorFname = firstName;
+                newAuthor.authorId = authors.size();
+                authors.add(newAuthor);
+                return newAuthor.authorId;
+            }
+        }
+        return -1;
+    }
+
+    public static void importData() {
+        for (int i = 0; i < Book_db.tokens.size(); i++) {
+            String[] temp = Book_db.tokens.get(i);
+            createAuthor(temp[2], temp[3], false);
+
+        }
+    }
+
 }
