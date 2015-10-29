d6dc6098e0a22634cbb3ecc7a3ce66bb81597bf0
bmathai <bmathai@unca.edu>
2015-10-29 08:39:41 -0400

    Added Filechooser
    uses java swing library

--- src/book_db/Book_db.java
+++ src/book_db/Book_db.java
@@ -8,6 +8,8 @@ package book_db;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import javax.swing.JDialog;
+import javax.swing.JFileChooser;
 
 /**
  *
@@ -19,8 +21,13 @@ public class Book_db {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws FileNotFoundException{
+        String currentPath = "";
+        final JFileChooser fc = new JFileChooser();
+        if (fc.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                currentPath = fc.getSelectedFile().getAbsolutePath();
+        }
         
-        String fileName = "C:\\Users\\Sean\\Documents\\NetBeansProjects\\book_db\\BookFile";
+        String fileName = currentPath;
         String line = null;
         
         Scanner infile = new Scanner(new File(fileName));
