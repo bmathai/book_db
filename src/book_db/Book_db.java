7f3d044bb42b4e38447348c70611e7948182b0be
Drew Proctor <alprocto@gmail.com>
2015-10-29 09:27:49 -0400

    change read in storage to ArrayList

--- src/book_db/Book_db.java
+++ src/book_db/Book_db.java
@@ -7,7 +7,7 @@ package book_db;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
 import javax.swing.JFileChooser;
 
 /**
@@ -20,7 +20,7 @@ public class Book_db {
      * @param args the command line arguments
      */
     
-    public static String[][] tokens = new String[100][20];
+    public static ArrayList<String[]> tokens = new ArrayList();
     
     public static void main(String[] args) throws FileNotFoundException{
         String currentPath = "";
@@ -33,27 +33,18 @@ public class Book_db {
         String line = null;
         
         Scanner infile = new Scanner(new File(fileName));
-        String tokensArr[] = new String[100];
-        int count = 0;
         while(infile.hasNextLine()) {
             line = infile.nextLine();
             System.out.println(line);
             //create array of individual entries without commas
+            String tokensArr[];
             tokensArr = line.split(",");
             System.out.println(tokensArr[0]);
-            tokens[count] = tokensArr;
-            count++;
-            System.out.println(count);
+            tokens.add(tokensArr);
+            System.out.println(tokens.size()-1);
             //System.out.println(line); //test to see if reading file correctly
         }
         
-        //blaise's work for publishers table
-        System.out.println(tokens[0][0]);
-        System.out.println(tokens[0][1]);
-        System.out.println(tokens[0][2]);
-        System.out.println(tokens[0][3]);
-        System.out.println(tokens[2][0]);
-        System.out.println(tokens[1][1]);
         
         
         
